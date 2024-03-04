36f7228a8fc94bdc0e651a76faebf54aa4777ec4
Ivan Kapitanov <ivorybananaa@gmail.com>
2024-03-04 22:11:55 +0300

    feat: add into, isSome, isNone to OptionAsync

--- src/option-async.ts
+++ src/option-async.ts
@@ -16,4 +16,56 @@ export class OptionTypeAsync<T> {
   ): Promise<A | B> {
     return this[Prom].then(onSuccess, onFailure);
   }
+
+  /**
+   * Return the contained `T`, or `none` if the option is `None`. The `none`
+   * value must be falsey and defaults to `undefined`.
+   *
+   * ```
+   * const x: Option<number> = Some(1);
+   * assert.equal(x.into(), 1);
+   *
+   * const x: Option<number> = None;
+   * assert.equal(x.into(), undefined);
+   *
+   * const x: Option<number> = None;
+   * assert.equal(x.into(null), null);
+   * ```
+   */
+  async into(this: OptionAsync<T>): Promise<T | undefined>;
+  async into<U extends FalseyValues>(this: OptionAsync<T>, none: U): Promise<T | U>;
+  async into(this: OptionAsync<T>, none?: FalseyValues): Promise<T | FalseyValues> {
+    return this.then((opt) => opt.into(none));
+  }
+
+  /**
+   * Returns true if the Option is `Some` and acts as a type guard.
+   *
+   * ```
+   * const x = Some(10);
+   * assert.equal(x.Is(), true);
+   *
+   * const x: Option<number> = None;
+   * assert.equal(x.Is(), false);
+   * ```
+   */
+  async isSome(this: OptionAsync<T>): Promise<boolean> {
+    return this.then((opt) => opt.isSome());
+  }
+
+  /**
+   * Returns true if the Option is `None` and acts as a type guard.
+   *
+   * ```
+   * const x = Some(10);
+   * assert.equal(x.isNone(), false);
+   *
+   * const x: Option<number> = None;
+   * assert.equal(x.isNone(), true);
+   * ```
+   */
+  async isNone(this: OptionAsync<T>): Promise<boolean> {
+    return this.then((opt) => opt.isNone());
+  }
+
 }
